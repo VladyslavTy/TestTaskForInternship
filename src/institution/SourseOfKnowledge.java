c2bae58363427413a00f7c78138be4169084d973
Vladyslav Tymofeiev <vladyslavty@interlink-ua.com>
2018-07-02 18:05:50 +0300

    added SourceOfKnowledge class

---
+++ src/institution/SourseOfKnowledge.java
@@ -0,0 +1,10 @@
+package institution;
+
+import person.Student;
+
+
+public abstract class SourseOfKnowledge {
+    public void givePracticalKnowledge(Student student){};
+    public void giveTheoryKnowledge(Student student){};
+
+}
